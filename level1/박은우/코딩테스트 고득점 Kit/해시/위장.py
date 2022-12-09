a85a924f2fa0dc02fd2c5f00206a8beba2ecb7ed
ieunune <une@kakao.com>
2022-12-09 18:32:17 +0900

    [programmes] 박은우 위장

--- level1/박은우/코딩테스트 고득점 Kit/해시/위장.py
+++ level1/박은우/코딩테스트 고득점 Kit/해시/위장.py
@@ -1,5 +1,22 @@
+from collections import defaultdict
+
+
 def solution(clothes):
-    return len(".".join(clothes))-1
+    # [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+    # ->
+    # {'headgear': ['yellow_hat', 'green_turban'], 'eyewear': ['blue_sunglasses']}
+
+    answer = 1
+    closet = defaultdict(list)
+
+    for item, category in clothes:
+        closet[category].append(item)
+
+    for item, category in closet.items():
+        answer *= len(category) + 1
+        print(item, category, 'answer : ', answer)
+
+    return answer -1
 
 # 테스트 케이스
 print('예제 1 : ', solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
